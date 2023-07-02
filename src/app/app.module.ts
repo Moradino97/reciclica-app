968ebe88a0735b1e4c64fb21de3ee1525a5ade12
InacioCarvalhoOliveira <inacio.carvalho.oliveira02@gmail.com>
2023-07-02 20:41:39 -0300

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
+import { AngularFireModule } from '@angular/fire/compat';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { StoreModule } from '@ngrx/store';
 import { AppStoreModule } from 'src/store/AppStoreModule';
 import { LoadingComponent } from './components/loading/loading.component';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [AppComponent ,LoadingComponent],
@@ -17,6 +17,7 @@ import { LoadingComponent } from './components/loading/loading.component';
     IonicModule.forRoot(), 
     AppRoutingModule, 
     StoreModule.forRoot({}, {}),
+    AngularFireModule.initializeApp(environment.firebaseConfig),
     ...AppStoreModule
   ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
